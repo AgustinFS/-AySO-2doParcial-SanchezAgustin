c0b1e87a7432b157279ea2879d7f571a5dc6d636
root <root@LVM2HDD>
2024-11-20 02:46:23 +0000

    subiendo punto B resuelto

---
+++ puntob.sh
@@ -0,0 +1,33 @@
+vmAMN316 (Nodo)
+1  sudo apt update
+    2  sudo apt install ansible
+    3  ll .ssh/
+    4  ssh-keygern
+    5  ssh-keygen
+    6  ll .ssh/
+    7  cat .ssh/id_rsa.pub
+    8  ip adress
+    9  ip address show
+   10  ssh vagrant@192.168.56.2
+   11  ssh vagrant@192.168.56.8
+   12  git clone https://github.com/upszot/UTN-FRA_SO_Ansible.git
+   13  ls
+   14  cd UTN-FRA_SO_Ansible/
+   15  ll
+   16  cd ejemplo_02/
+   17  vim inventory
+   18  cat playbook.yml
+   19  vim playbook.yml
+   20  cat playbook.yml
+   21  ansible-playbook -i inventory playbook.yml
+   22  ssh vagrant@192.168.56.8
+   23  history
+
+vmAnsDev316
+1  ip address show
+    2  ll .ssh/
+    3  echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDQR8NUqrbw6Up/44kTP076h7qDn92Bbwv5NyInEgmjF5u2PDiqFqp+YBysmDzF0YG1ayLWWZQ76uOAQQRD4EGG7FsQ0BKnBgJFGoyNnV4vUBtHwuXNc7qhqgw6v0buGnWwtyRnWh3tmvmSHiPKqthNeWc4tTelsFL8c2lU6NwePrpC9Q1nFIZR50D0Ej4hceeuVY5AQEShfj/JoaU+zySvZ/dmH5o29zOKzBBVZFknHxwBoGGK4+EiVTye6Gqe8pkK6rhGlbt1PfbE+LTA2xyWu3EuF6RUC/zJCC1J0rHw2ELC5jQVcMbZmZEcL4HibHKI0Q6oDkqeKRUYNFerdG+NZbp79u+GtYe7B8xNFVk6PXKW8jncGLQSyHoW5OutkjX9sgzZ27nmhlBIAOuOODNb0PL2Nwk2lB+f4h7zMlWNDe6XF+AAX64ikIwaAgau2zh4jEQqFg4uGE5LIi9WyOHoms4qA1w51TCqFbBTaFZUD9xeWWGVaKAfZJ//aRUixAU= vagrant@vmAMN213" >> .ssh/authorized_keys
+    4  cat .ssh/authorized_keys
+    5  ip address show
+    6  history
+
